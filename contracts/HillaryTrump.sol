4d2ee1d472549bcfb9904eada635308f657b526f
Axel Hodler <derxor@gmail.com>
2016-11-01 11:03:52 +0100

    mark functions as payable

--- contracts/HillaryTrump.sol
+++ contracts/HillaryTrump.sol
@@ -9,13 +9,13 @@ contract HillaryTrump {
     oracle = tx.origin;
   }
 
-  function trump() {
+  function trump() payable {
     if (msg.value == 10000000000000000000) {
       trumpBetter = msg.sender;
     }
   }
 
-  function hillary() {
+  function hillary() payable {
     if (msg.value == 40000000000000000000) {
       hillaryBetter = msg.sender;
     }
